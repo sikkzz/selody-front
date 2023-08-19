79cda83741bc5b502ec37c153d6ae108ebd4865e
sikkzz <sling0623@gmail.com>
2023-08-19 20:37:33 +0900

    #115 design: Feed LandingPage styling

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-import FeedMain from "@/components/FeedPage/FeedMain/FeedMain";
-import FeedHeader from "@/components/Header/FeedHeader/FeedHeader";
+// import FeedMain from "@/components/FeedPage/FeedMain/FeedMain";
+// import FeedHeader from "@/components/Header/FeedHeader/FeedHeader";
+
+import FeedLanding from "@/components/Feed/FeedLanding/FeedLanding";
 import Header from "@/components/Header/Header/Header";
 
 const FeedPage = () => {
 	return (
 		<>
 			<Header />
-			<FeedHeader />
-			<FeedMain />
+			<FeedLanding />
+			{/* <FeedHeader /> */}
+			{/* <FeedMain /> */}
 		</>
 	);
 };
