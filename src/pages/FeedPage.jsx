cfd24971ba41db89f6e705fdc3e929a046fe2bd6
sikkzz <sling0623@gmail.com>
2023-08-29 18:56:25 +0900

    #115 refactor: feed header, main delete

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 
-// import FeedMain from "@/components/FeedPage/FeedMain/FeedMain";
-// import FeedHeader from "@/components/Header/FeedHeader/FeedHeader";
-
 import FeedLanding from "@/components/Feed/FeedLanding/FeedLanding";
 
 const FeedPage = () => {
-	return (
-		<>
-			<FeedLanding />
-			{/* <FeedHeader /> */}
-			{/* <FeedMain /> */}
-		</>
-	);
+	return <FeedLanding />;
 };
 export default FeedPage;
